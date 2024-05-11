827949e11c9fe32feb10e1fa4d25bd9d5d40f0e8
Karla Ramírez Márquez <116531477+WolfkarlX@users.noreply.github.com>
2024-05-11 15:04:01 -0600

    Confirmar en el botón para eliminar usuarios (#15)
    * Diseño agregado a las alerts/confirm
    Cambios en la DB: se agregaron todos los temas enumerados del 1 al 17
    Detalles de ortografía corregidos
    Contenedor de publicación corregido
    Ya se ve la imagen de la publucación cuando abres la publicación
    
    * Merge main-development
    
    * Se agregó una alert para el botón "Eliminar" de usuarios

--- public/resources/js/sweetalert.js
+++ public/resources/js/sweetalert.js
@@ -1,3 +1,4 @@
+/* Confirmar que se ha creado el usuario*/
 function usercreado(){
     Swal.fire({
         title: "¡Usuario creado!",
@@ -10,16 +11,18 @@ function usercreado(){
     });
 }
 
+/* Confirmar que se ha creado la publicación */
 function publicreada(){
     Swal.fire({
                        
         icon: "success",
         title: "Se ha creado la publicación",
         showConfirmButton: false,
-        timer: 1500
+        timer: 1000
     });
 }
 
+/* Avisar que no se creó la publicación */
 function nocreada(){
     Swal.fire({
         title: "¡No se pudo crear la publicación!",
@@ -32,6 +35,7 @@ function nocreada(){
     });
 }
 
+/* Invitar al visitante a iniciar sesión */
 function entravisit(){
     Swal.fire({
         title: "¡Inicia sesión para interactuar!",
@@ -43,5 +47,24 @@ function entravisit(){
         if (result.isConfirmed) {
             app.view("login");
         }
+    });
+}
+
+/* Cuestionar al Admin si desea eliminar a un usuario seleccionado */
+function deleteuser(){
+    Swal.fire({
+        icon:"info",
+        title: "¿Está seguro de eliminar a este usuario?",
+        text: "Una vez eliminado no podrá revertir esta acción",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Eliminar",
+        denyButtonText: `No eliminar`
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire("¡Usuario eliminado!", "", "success");
+        } /*else if (result.isDenied) {
+            Swal.fire("Usuario no eliminado", "", "info");
+        }*/
     });
 }
