308b5dfa784e5ba5f5d4d402e9a89fa5b80dd5c7
Karla Kamila Vilchis Corona <112263019+Kamila04@users.noreply.github.com>
2024-05-20 16:46:50 -0600

    Forgot Password Awesome Feature (#17)
    Co-authored-by: Karla Ramírez Márquez <116531477+WolfkarlX@users.noreply.github.com>

--- app/classes/Router.php
+++ app/classes/Router.php
@@ -10,6 +10,7 @@ use app\controllers\PerfilController as Perfil;
 use app\controllers\AdminuserController as Adminuser;
 use app\controllers\AdminpublicController as Adminpublic;
 use app\controllers\PostController as Post;
+use app\controllers\ForgotController as Forgot; 
 use app\controllers\UserController as User;
 
 class Router {
@@ -38,6 +39,8 @@ class Router {
                 $controller = new Adminpublic(); break;
             case 'AdminuserController':
                 $controller = new Adminuser(); break;
+            case 'ForgotController':
+                $controller = new Forgot(); break;
             case 'UserController':
                 $controller = new User(); break;
             default :
