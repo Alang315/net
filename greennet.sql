bab8b2be1195f44bbbc9d6f60433506c80d0fad0
karla <karlarmlp@gmail.com>
2024-05-28 03:38:01 -0600

    DB con los datos más recientes

--- greennet.sql
+++ greennet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-05-2024 a las 19:02:21
+-- Tiempo de generación: 28-05-2024 a las 11:36:34
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -66,7 +66,12 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`ID_comment`, `Content`, `Date`, `ID_user`, `ID_publication`) VALUES
-(4, 'Me encanta', '2024-05-07 19:48:54', 2, 5);
+(5, '¡Amando siempre en todas las estaciones! ❤️', '2024-05-27 11:58:10', 3, 8),
+(6, 'Demasiado calor ', '2024-05-28 08:31:41', 3, 5),
+(7, 'Odio el calor', '2024-05-28 12:34:59', 1, 5),
+(8, 'A mí me da miedo que las empresas sigan gastando litros y litros de agua y que el gobierno intenta que nosotros seamos los que ahorremos', '2024-05-28 12:56:04', 2, 13),
+(9, 'Ríos y lagunas secas, donde antes paseaban en lancha, ahora pasan caminando 😢', '2024-05-28 12:56:52', 4, 13),
+(10, 'Comparto ese encanto por la naturaleza, es tan hermosa ❤️🍃', '2024-05-28 01:08:42', 2, 2);
 
 -- --------------------------------------------------------
 
@@ -98,11 +103,10 @@ INSERT INTO `publication` (`ID_publication`, `Title`, `Content`, `Date`, `Active
 (7, 'De Basura a Energía', 'Con los residuos sólidos urbanos se puede generar energía térmica (agua o aire caliente y vapor), energía eléctrica e incluso mecánica mediante el uso de biocarburantes en motores de combustión interna mediante el procedimiento apropiado de transformación.', ' 05-05-2024 07:58 pm', 1, 2, 7, 'basura_a_energia.jpg'),
 (8, 'El amor no es de a ratos', 'El amor debe de ser y estar durante todas las estaciones, no solo en primavera cuando más florece, si no también en verano, cuando más cansancio se siente por el calor, también en otoño, cuando todas esas hojas y flores hermosas se han ido, y en invierno, cuando más calor se necesita', ' 07-05-2024 09:58 am', 1, 2, 15, 'estaciones.gif'),
 (9, '¡JUSTICIA!', '¡Justicia para todas aquellas personas que no inician una guerra pero son quienes pagan las consecuencias!, A todas esas personas que no pueden si quiera vivir tranquilas sabiendo que en cualquier momento sus gobernantes iniciarán batallas y ellos se irán a esconder porque son unos cobardes, unos cobardes que no se entregan, unos cobardes que avientan la piedra y esconden la mano.', ' 10-05-2024 12:18 am', 1, 2, 16, NULL),
-(10, 'Feliz cumpleaños', 'Feliz cumple a quien sea que cumpla hoy', ' 10-05-2024 12:49 am', 1, 1, 17, NULL),
 (11, 'Siempre recordemos a quienes nos ayudaron', 'Aún recuerdo que cuando era niño hubo una vez que estaba teniendo un mal día, en la casa mi familia estaba peleada, yo ni desayuné y en la escuela tenía mucha hambre, no me dieron para gastar ni llevé comida, pero ahí estuvo Kito, me compartió comida y me invitó a su casa luego de clases, sus padres eran amigos de los míos, sabían lo que pasaba en casa y me llevaron a su casa para no tener que lidiar con problemas de adultos, estuve todo ese día jugando con Kito, un gran amigo de siempre. Muchas gracias Kito, te quiero mucho  ❤️', ' 10-05-2024 08:03 pm', 1, 4, 3, NULL),
 (12, 'Kito, mi hermano', 'En serio no sé cómo agradecerte por todo lo que has hecho por mí 😢❤️‍🩹 sólo sé que ahora que necesitas de mi apoyo, aquí estoy y no sabes la impotencia que me da no poder hacer más por ti, pero en serio, cualquier apoyo económico o moral que ocupes, aquí estoy.', ' 10-05-2024 08:03 pm', 1, 4, 3, NULL),
-(13, 'EN SERIO, HACE CALOR', 'Gente, ¿de verdad no les asusta el hecho de que hace cada vez más calor y que lo repelemos con electricidad y que la electricidad comienza a fallar y en es probable que en algún punto no tengamos nada para lidiar con el calor? y ni hablar del agua que está escaseando, en serio, hay un problema.', ' 10-05-2024 08:13 pm', 0, 3, 13, NULL),
-(14, 'No puedo cambiar todo el mundo, pero puedo cambiar el pedacito que me toca ❤️‍🩹', '.', ' 10-05-2024 08:20 pm', 0, 2, 15, 'plantando_arbolito.jpeg');
+(13, 'EN SERIO, HACE CALOR', 'Gente, ¿de verdad no les asusta el hecho de que hace cada vez más calor y que lo repelemos con electricidad y que la electricidad comienza a fallar y en es probable que en algún punto no tengamos nada para lidiar con el calor? y ni hablar del agua que está escaseando, en serio, hay un problema.', ' 10-05-2024 08:13 pm', 1, 3, 13, NULL),
+(14, 'No puedo cambiar todo el mundo, pero puedo cambiar el pedazito que me toca ', '🌱❤️‍🩹', ' 28-05-2024 03:30 am', 0, 2, 15, 'plantando_arbolito.jpeg');
 
 -- --------------------------------------------------------
 
@@ -131,6 +135,31 @@ CREATE TABLE `reactions_publications` (
   `ID_publication` int(11) NOT NULL,
   `Date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `reactions_publications`
+--
+
+INSERT INTO `reactions_publications` (`ID_reaction`, `ID_user`, `ID_type`, `ID_publication`, `Date`) VALUES
+(37, 4, 6, 2, '0000-00-00'),
+(38, 4, 4, 3, '0000-00-00'),
+(39, 4, 6, 4, '0000-00-00'),
+(40, 4, 6, 7, '0000-00-00'),
+(41, 4, 6, 8, '0000-00-00'),
+(42, 4, 3, 9, '0000-00-00'),
+(44, 3, 4, 3, '0000-00-00'),
+(45, 3, 3, 5, '0000-00-00'),
+(46, 3, 6, 7, '0000-00-00'),
+(47, 3, 6, 8, '0000-00-00'),
+(48, 3, 4, 9, '0000-00-00'),
+(49, 3, 6, 11, '0000-00-00'),
+(50, 3, 6, 12, '0000-00-00'),
+(52, 2, 6, 4, '0000-00-00'),
+(53, 2, 6, 8, '0000-00-00'),
+(54, 2, 6, 11, '0000-00-00'),
+(55, 2, 6, 12, '0000-00-00'),
+(56, 2, 3, 13, '0000-00-00'),
+(57, 2, 6, 2, '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -235,9 +264,7 @@ INSERT INTO `user` (`ID_user`, `Username`, `Password`, `Email`, `Biography`, `ID
 (1, 'Administrador', '$2y$10$okb1t2xizW4JicC1JOTm/.UUYrriyZZXTLQbzlCegm0cph/IXUM2m', 'admin@greennet.com', NULL, 1),
 (2, 'Wolfkarl', '$2y$10$okb1t2xizW4JicC1JOTm/.UUYrriyZZXTLQbzlCegm0cph/IXUM2m', 'karlarmlp@gmail.com', NULL, 2),
 (3, 'Alan VP', '$2y$10$okb1t2xizW4JicC1JOTm/.UUYrriyZZXTLQbzlCegm0cph/IXUM2m', 'avargas39@ucol.mx', NULL, 2),
-(4, 'Gabito', '$2y$10$okb1t2xizW4JicC1JOTm/.UUYrriyZZXTLQbzlCegm0cph/IXUM2m', 'lramirez68@ucol.mx', NULL, 2),
-(5, 'Alguien', '$2y$10$okb1t2xizW4JicC1JOTm/.UUYrriyZZXTLQbzlCegm0cph/IXUM2m', 'alguien@gmail.com', NULL, 2),
-(33, 'KARLA RM', '$2y$10$tBdDWjmw5MrFQBOjg6CLEumjN5fO3E79Z1wJhUcjC6Hibfi.OmKRG', 'kramirez32@ucol.mx', NULL, 2);
+(4, 'Gabito', '$2y$10$okb1t2xizW4JicC1JOTm/.UUYrriyZZXTLQbzlCegm0cph/IXUM2m', 'lramirez68@ucol.mx', NULL, 2);
 
 --
 -- Índices para tablas volcadas
@@ -326,13 +353,13 @@ ALTER TABLE `codes`
 -- AUTO_INCREMENT de la tabla `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `ID_comment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_comment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `publication`
 --
 ALTER TABLE `publication`
-  MODIFY `ID_publication` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+  MODIFY `ID_publication` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
 
 --
 -- AUTO_INCREMENT de la tabla `reactions_comments`
@@ -344,7 +371,7 @@ ALTER TABLE `reactions_comments`
 -- AUTO_INCREMENT de la tabla `reactions_publications`
 --
 ALTER TABLE `reactions_publications`
-  MODIFY `ID_reaction` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `ID_reaction` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT de la tabla `reaction_type`
@@ -362,13 +389,13 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT de la tabla `topics`
 --
 ALTER TABLE `topics`
-  MODIFY `ID_topic` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `ID_topic` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `ID_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- Restricciones para tablas volcadas
